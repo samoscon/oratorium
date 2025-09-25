9d1fc20c57ae1a10ef2cccce836ff7804a924b17
samoscon <53895420+samoscon@users.noreply.github.com>
2025-09-25 14:15:24 +0200

    Update autoload.php
    Adding vendor name to autoload file

--- controllerFramework/autoload.php
+++ controllerFramework/autoload.php
@@ -3,13 +3,13 @@ spl_autoload_register(function ($class_name) {
     if(preg_match('/\\\\/', $class_name)) {
         $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
     }
-    if(file_exists("vendor".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."controllerFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
-        require_once "vendor".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."controllerFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
+    if(file_exists("vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."controllerFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
+        require_once "vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."controllerFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
     }
     if(file_exists("MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
         require_once "MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
     }
-    if(file_exists("vendor".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."membersactivitiesFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
-        require_once "vendor".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."membersactivitiesFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
+    if(file_exists("vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."membersactivitiesFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
+        require_once "vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."membersactivitiesFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
     }
 });
